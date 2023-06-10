1f99233e72c236597ce23e17e998d7e55dfabd78
Deadpool1 <atwood169@yahoo.com>
2023-06-10 00:31:21 -0500

    test react

---
+++ src/components/General.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class General extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Hello World</h1>
+            </div>
+        )
+    }
+}
+
+export default General
