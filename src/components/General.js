468747812a098b5e8e34b6948405fcd85a1dc88f
Deadpool1 <atwood169@yahoo.com>
2023-06-12 16:22:02 -0500

    added method to general

--- src/components/General.js
+++ src/components/General.js
@@ -1,16 +1,54 @@
 import React, { Component } from "react";
 
 class General extends Component {
+    constructor(props) {
+        super(props) 
+        this.state = {
+            firstName: '',
+            lastName: '',
+            email: '',
+            phoneNumber: '',
+        }
+    }
+
+    handleInputChange = (e) => {
+        const { name, value } = e.target
+        this.setState({ [name]: value })
+    }
 
     render() {
         return (
             <div className="header-info">
                 <h2>General</h2>
                 <div className="input-container">
-                    <input placeholder="First Name" style={{ border: 'none'}}></input>
-                    <input placeholder="Last Name" style={{ border: 'none'}}></input>
-                    <input placeholder="Email" style={{ border: 'none'}}></input>
-                    <input placeholder="Phone number" style={{ border: 'none'}}></input>
+                    <input 
+                        placeholder="First Name" 
+                        style={{ border: 'none'}}
+                        name="firstName"
+                        value={this.state.firstName}
+                        onChange={this.handleInputChange}
+                    />
+                    <input 
+                        placeholder="Last Name" 
+                        style={{ border: 'none'}}
+                        name="lastName"
+                        value={this.state.lastName}
+                        onChange={this.handleInputChange}
+                        />
+                    <input 
+                        placeholder="Email" 
+                        style={{ border: 'none'}}
+                        name="email"
+                        value={this.state.email}
+                        onChange={this.handleInputChange}
+                    />
+                    <input 
+                        placeholder="Phone number" 
+                        style={{ border: 'none'}}
+                        name="phoneNumber"
+                        value={this.state.phoneNumber}
+                        onChange={this.handleInputChange}
+                    />
                 </div>
                 
             </div>
