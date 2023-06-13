c8e1bb19c12a69c0867810596d4fccad206806d4
Deadpool1 <atwood169@yahoo.com>
2023-06-13 13:45:55 -0500

    updated exp

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,16 +1,50 @@
 import React, { Component } from "react";
 
 class Experience extends Component {
-    //add contructor and create state method
+    constructor(props) {
+        super(props)
+        this.state = {
+            company: '',
+            position: '',
+            startDate: '',
+            endDate: ''
+        }
+    }
+
+    handleInputChange = (e) => {
+        const { name, value } = e.target
+        this.setState({ [name]: value })
+    }
+    
     render() {
         return (
             <div className="info-header">
                 <h2>Experience</h2>
                 <div className="input-container">
-                    <input placeholder="Company" style={{ border: 'none' }}></input>
-                    <input placeholder="Position" style={{ border: 'none' }}></input>
-                    <input placeholder="Start Date YYYY" style={{ border: 'none' }}></input>
-                    <input placeholder="End Date YYYY" style={{ border: 'none' }}></input>
+                    <input 
+                        placeholder="Company" 
+                        style={{ border: 'none' }}
+                        value={this.state.company}
+                        onChange={this.handleInputChange}
+                    />
+                    <input 
+                        placeholder="Position" 
+                        style={{ border: 'none' }}
+                        value={this.state.position}
+                        onChange={this.handleInputChange}
+                    />
+                    <input 
+                        placeholder="Start Date YYYY" 
+                        style={{ border: 'none' }}
+                        value={this.state.startDate}
+                        onChange={this.handleInputChange}
+                />
+                    <input 
+                        placeholder="End Date YYYY" 
+                        style={{ border: 'none' }}
+                        value={this.state.endDate}
+                        onChange={this.handleInputChange}
+                    />
                 </div>
             </div>
         )
