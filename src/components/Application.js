b439926f260cb46b3010f3e4487cc0fd5f2948af
Deadpool1 <atwood169@yahoo.com>
2023-06-13 14:22:25 -0500

    set default data value

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,9 +2,14 @@ import React, { Component } from "react";
 
 class Application extends Component {
     render() {
+        const { data } = this.props
+        const { firstName, lastName, email, phoneNumber } = data || {};
         return (
-            <div>
-                
+            <div className="application">
+                <h2>Application</h2>
+                <p>Name: {firstName} {lastName}</p>
+                <p>Email: {email}</p>    
+                <p>Phone Number: {phoneNumber}</p>
             </div>
         )
     }
