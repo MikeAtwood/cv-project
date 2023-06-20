d501d1142d1e0e37265718c41db265773316689d
Deadpool1 <atwood169@yahoo.com>
2023-06-20 14:55:29 -0500

    updated NewForm

--- src/components/NewForm.js
+++ src/components/NewForm.js
@@ -1,25 +1,37 @@
 import React, { Component } from "react";
-import General from "./General";
 
 class NewForm extends Component {
 
 
     render() {
-        const { firstName, lastName, email, phoneNumber, handleInputChange} = this.props
+        const { firstName, lastName, email, phoneNumber} = this.props
         return (
             <div className="input-field">
                 <div className="input-info">
-                    <General
-                        firstName={firstName}
-                        lastName={lastName}
-                        email={email}
-                        phoneNumber={phoneNumber}
-                        handleInputChange={handleInputChange}
+                    <input 
+                        type="text"
+                        name="firstName"
+                        value={firstName}
+                        readOnly
                     />
-                    {/* <p>First Name: {firstName}</p>
-                    <p>Last Name: {lastName}</p>
-                    <p>Email: {email}</p>
-                    <p>Phone Number: {phoneNumber}</p> */}
+                    <input 
+                        type="text"
+                        name="lastName"
+                        value={lastName}
+                        readOnly
+                    />
+                    <input 
+                        type="email"
+                        name="email"
+                        value={email}
+                        readOnly
+                    />
+                    <input 
+                        type="tel"
+                        name="phoneNumber"
+                        value={phoneNumber}
+                        readOnly
+                    />
                 </div>
             </div>
         )
