cbf76e92917456588ae486ed4279210644d513fe
Deadpool1 <atwood169@yahoo.com>
2023-06-25 14:20:28 -0500

    added add btn in Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,17 +7,43 @@ class Form extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            firstName: '',
-            lastName: '',
-            email: '',
-            phoneNumber: '',
-            schoolName: '',
+            numExperiences: 0, // start with one Experience section
+            maxExperiences: 5,
+            experiences: [{ company: '', position: '', tasks: '', startDate: '', endDate: '' }]
         }
     }
 
-    
+    handleAddExperience = () => {
+        this.setState(prevState => ({
+            numExperiences: prevState.numExperiences + 1,
+            experiences: [...prevState.experiences, { company: '', position: '', tasks: '', startDate: '', endDate: '' }]
+        }))
+    }
+
+    handleExperienceChange = (index, event) => {
+        const { name, value } = event.target
+        const experiences = [...this.state.experiences]
+        experiences[index][name] = value
+        this.setState({ experiences })
+    }
 
     render() {
+        const { numExperiences, experiences } = this.state
+        const experienceSections = []
+        for (let i = 0; i < numExperiences; i++) {
+            experienceSections.push(
+                <Experience
+                    key={i}
+                    index={i}
+                    company={experiences[i].company}
+                    position={experiences[i].position}
+                    tasks={experiences[i].tasks}
+                    startDate={experiences[i].startDate}
+                    endDate={experiences[i].endDate}
+                    onExperienceChange={this.handleExperienceChange}
+                />
+            )
+        }
         return (
             <div>
                 
@@ -31,7 +57,8 @@ class Form extends Component {
                     <Experience
                         handleInputChange={this.props.handleInputChange}
                     />
-                    
+                     {experienceSections}
+                     <button onClick={this.handleAddExperience}>Add+</button>
                 </div>
             </div>
         )
